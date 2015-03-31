3a9ac3541e292cb5ec4d8d518bf94a267efc8d58
Jerome Zago <jerome.zago@gmail.com>
2015-04-01 01:10:11 +0200

    Add more test files
    Actually they correspond to games referenced in "Beginning Arimaa",
    Appendix C (figures 71-75).

--- src/testmovelist.cpp
+++ src/testmovelist.cpp
@@ -21,11 +21,12 @@ void TestMoveList::plies_data()
     QTest::addColumn<int>("plies");
 
     QDir dir(QStringLiteral(QT_TESTCASE_BUILDDIR) + QLatin1String("/testdata"));
-    QList<int> plies { 92 };
+    QList<int> plies { 122, 61, 82, 134, 92 };
 
     for (auto &entry: dir.entryList(QDir::Files))
         QTest::newRow(qPrintable(entry)) << dir.filePath(entry)
                                          << plies.takeFirst();
+    QVERIFY(plies.isEmpty());
 }
 
 QTEST_MAIN(TestMoveList)
