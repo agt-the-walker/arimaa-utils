60e4380be00f9dd3a22999b19d24615c6360d075
Jerome Zago <jerome.zago@gmail.com>
2015-04-03 13:42:26 +0200

    Take takebacks into account

--- lib/movelist.rb
+++ lib/movelist.rb
@@ -4,6 +4,9 @@ class MoveList
   attr_reader :plies
 
   def initialize(movelist)
-    @plies = movelist.scan('\n').length
+    @plies = -1  # movelist typically ends with 39w or 50b, which can be ignored
+    movelist.split('\n').each do |move|
+      @plies += move.end_with?('takeback') ? -1 : 1
+    end
   end
 end
