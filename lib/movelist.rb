ed6c7b36cba63d64de2446c22a0972fbc38d1f69
Jerome Zago <jerome.zago@gmail.com>
2015-04-03 15:10:38 +0200

    Compute FEN of initial position setups

--- lib/movelist.rb
+++ lib/movelist.rb
@@ -1,12 +1,65 @@
 #!/usr/bin/env ruby
 
 class MoveList
-  attr_reader :plies
+  BOARD_SIZE = 8
+  COLUMN_RANGE = Range.new('a', ('a'.ord + BOARD_SIZE).chr, true)
+  ROW_RANGE = Range.new(1.to_s, BOARD_SIZE.to_s)
 
   def initialize(movelist)
-    @plies = -1  # movelist typically ends with 39w or 50b, which can be ignored
+    @board = {}  # key: square (for instance e4)
+
+    @moves = []
     movelist.split('\n').each do |move|
-      @plies += move.end_with?('takeback') ? -1 : 1
+      if move.end_with?('takeback')
+        @moves.pop
+      else
+        @moves << move.sub(/^\w+\s+/, '')  # remove first word (move number)
+      end
     end
+    @moves.pop  # movelist typically ends with 39w or 50b, which can be ignored
+  end
+
+  def plies
+    @moves.size
+  end
+
+  def each
+    # http://blog.arkency.com/2014/01/ruby-to-enum-for-enumerator/
+    return enum_for(:each) unless block_given?
+
+    @moves.each_with_index do |move, ply|
+      move_number = "#{ply / 2 + 1}#{ply.even? ? 'g' : 's'}"
+      move.split(' ').each do |step|
+        @board[step[1..2]] = step[0]
+      end
+      yield "#{move_number} #{fen}"
+    end
+  end
+
+private
+
+  def fen
+    ROW_RANGE.collect do |row|
+      buffer = ''
+      empty_nb = 0
+
+      COLUMN_RANGE.each do |column|
+        if piece = @board[column + row]
+          if empty_nb > 0
+            buffer << empty_nb.to_s
+            empty_nb = 0
+          end
+          buffer << piece
+        else
+          empty_nb += 1
+        end
+      end
+
+      if empty_nb > 0
+        buffer << empty_nb.to_s
+      end
+
+      buffer
+    end.reverse.join('/')
   end
 end
--- lib/test_movelist.rb
+++ lib/test_movelist.rb
@@ -4,12 +4,31 @@ require 'test/unit'
 require_relative 'movelist'
 
 class TestMoveList < Test::Unit::TestCase
+  def setup
+    unless defined?(@@move_lists)
+      @@move_lists = {}
+      Dir['testdata/*.txt'].sort.each do |path|
+        @@move_lists[File.basename(path, '.txt').to_i] =
+            MoveList.new(IO.read(path))
+      end
+    end
+  end
+
   def test_plies
     expected_plies = [28, 9, 76, 75, 122, 61, 82, 134, 92]
-    Dir['testdata/*.txt'].sort.each do |path|
-      assert_equal(expected_plies.shift, MoveList.new(IO.read(path)).plies,
-                   path)
+    @@move_lists.each do |game_id, move_list|
+      assert_equal(expected_plies.shift, move_list.plies, game_id)
     end
     assert_empty(expected_plies)
+  end
+
+  def test_fen_setup
+    [[33, ['1g 8/8/8/8/8/8/HDRMECDH/RRRRRRCR',
+           '1s rrcrrcrr/hdremrdh/8/8/8/8/HDRMECDH/RRRRRRCR']],
+     [138, ['1g 8/8/8/8/8/8/HDCMECDH/RRRRRRRR',
+            '1s rrrrerrr/hdcmrcdh/8/8/8/8/HDCMECDH/RRRRRRRR']]
+    ].each do |game_id, fens|
+      assert_equal(fens, @@move_lists[game_id].each.first(2))
+    end
   end
 end
