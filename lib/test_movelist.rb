95900c7163aa8c03e867c5d8b7e0de2134bc4e89
Jerome Zago <jerome.zago@gmail.com>
2015-04-05 22:51:58 +0200

    Add more test files for missing piece types

--- lib/test_movelist.rb
+++ lib/test_movelist.rb
@@ -15,7 +15,9 @@ class TestMoveList < Test::Unit::TestCase
   end
 
   def test_plies
-    expected_plies = [28, 9, 76, 75, 122, 71, 61, 109, 78, 82, 134, 92, 17]
+    expected_plies = [28, 9, 76, 75, 129, 77, 65, 73, 137, 128,   # 00033.txt...
+                      122, 71, 61, 106, 154, 109, 78, 82, 134, 92,# 25021.txt...
+                      17]                                         # 90687
     @@move_lists.each do |game_id, move_list|
       assert_equal(expected_plies.shift, move_list.plies, game_id)
     end
@@ -41,6 +43,30 @@ class TestMoveList < Test::Unit::TestCase
 
      [3917, '38s 1r3R1r/1Dr3r1/r7/RHeE1H2/r7/R3r3/3R1MC1/2R2hRR'],
 
+     # with elephants, camels and dogs but no horses or cats
+     [4296, '65g 1r6/4rdr1/8/r6r/2mRe2R/4E3/5D2/8',
+            '65g 6r1/1rcr4/8/r6r/R2hRd2/3H4/2C5/8'],
+
+     # with elephants, camels and cats but no horses or dogs
+     [4326, '30s rr3r1r/1c1c2rr/1C1E2eM/8/8/4R2R/1R3CR1/R3RR2',
+            '30s r1r3rr/rr2c1c1/Dh2H1C1/8/8/R2R4/1RC3R1/2RR3R'],
+
+     # with elephants, horses and dogs but no camels or cats
+     [6780, '26g 8/2r1r3/1r1e1rrr/d7/R1E5/1H4R1/2D2D1R/RR1R1R2',
+            '26g 8/3r1r2/rrr1h1r1/7c/5H1R/1R4D1/R1C2C2/2R1R1RR'],
+
+     # with elephants, dogs and cats but no camels or horses
+     [6873, '23s rr1r1r2/c4c2/1r1r2rr/2e5/5R2/3CD1Ed/2D4R/RRR2R1R',
+            '23s 2r1r1rr/2c4c/rr2r1r1/5h2/2R5/dH1DC3/R4D2/R1R2RRR'],
+
+     # with elephants and dogs but no camels, horses or cats
+     [7371, '64s 8/8/1e4Er/4d2R/4R1rR/6D1/8/8',
+            '64s 8/8/rD4d1/R2c4/Rr1R4/1C6/8/8'],
+
+     # with elephants, horses and cats but no camels or dogs
+     [14013, '64s r7/R4r2/h2c2c1/R1e1R3/8/1r5C/5E1r/7R',
+             '64s r7/R4r2/d2c2c1/R1h1R3/8/1r5C/5H1r/7R'],
+
      # row 1 symmetrical
      [25021, '2s rrrddrrr/rhcemchr/8/8/3E4/6H1/CMDH1D1C/RRRRRRRR',
              '2s rrrddrrr/rhcmechr/8/8/4E3/1H6/C1D1HDMC/RRRRRRRR'],
@@ -49,7 +75,15 @@ class TestMoveList < Test::Unit::TestCase
      [40260, '25s 3crch1/1rrrrHrr/3EdMHr/dhmReRRR/RDC1C3/RD6/2R2R2/8',
              '25s 1hcrc3/rrHrrrr1/rHMdE3/RRReRmhd/3C1CDR/6DR/2R2R2/8'],
 
-     # completely symmetrical, with dogs but no cats
+     # with elephants and cats but no camels, horses or dogs
+     [43717, '46g 8/r4r2/4cRc1/r1e2E1r/R3R3/1R6/6C1/6R1',
+             '46g 8/2r4r/1cRc4/r1D2d1r/3R3R/6R1/1C6/1R6'],
+
+     # rows 1 to 5 symmetrical, with elephants only (and rabbits)
+     [47721, '73g 4r2r/8/3E2e1/8/8/8/8/8',
+             '73g r2r4/8/1c2C3/8/8/8/8/8'],
+
+     # completely symmetrical, with dogs only (and rabbits)
      [75005, '40g rrrrrrrr/8/8/8/8/8/2d2d2/8',
              '40g rrrrrrrr/8/8/8/8/8/2c2c2/8'],
 
