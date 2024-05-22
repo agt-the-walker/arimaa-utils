6acf5a51e94e329fe66fe3f514ef8c2aaf8043f5
Jerome Zago <jerome.zago@gmail.com>
2024-05-22 23:03:35 +0200

    Ran "rubocop --only Style/Next -A

--- lib/movelist.rb
+++ lib/movelist.rb
@@ -90,19 +90,17 @@ class MoveList
 
     (PECKING_ORDER.index(piece) + 1).upto(PECKING_ORDER.size - 1) do |index|
       stronger_piece = PECKING_ORDER[index]
-      if nb_pieces[stronger_piece]
-        weaker_piece = PECKING_ORDER[index - 1]
-        nb_pieces[weaker_piece] = nb_pieces.delete(stronger_piece)
+      next unless nb_pieces[stronger_piece]
+      weaker_piece = PECKING_ORDER[index - 1]
+      nb_pieces[weaker_piece] = nb_pieces.delete(stronger_piece)
 
-        @board.each do |square, piece|
-          if piece.downcase == stronger_piece
-            @board[square] = if piece =~ /[A-Z]/
-                               weaker_piece.upcase
-                             else
-                               weaker_piece
-                             end
-          end
-        end
+      @board.each do |square, piece|
+        next unless piece.downcase == stronger_piece
+        @board[square] = if piece =~ /[A-Z]/
+                           weaker_piece.upcase
+                         else
+                           weaker_piece
+                         end
       end
     end
   end
